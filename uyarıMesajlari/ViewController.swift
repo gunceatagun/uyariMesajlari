2fcdfbca7bc003fdb9ebb089d3e98f105f6ff32a
günce <gncdmn.95@icloud.com>
2023-05-25 15:27:42 +0300

    Initial Commit

---
+++ uyarıMesajlari/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  uyarıMesajlari
+//
+//  Created by Günce ATAGÜN on 25.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
